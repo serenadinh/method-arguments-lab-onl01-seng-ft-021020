8fa4788da2363ec75b46aa92de9309b36e01f2f1
heyserenadinh Dinh <heyserenadinh@gmail.com>
2020-02-17 19:05:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,5 @@
 
-def introduction (name, language)
-  @name = "name"
-  @language = "language"
-end
+
 def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
